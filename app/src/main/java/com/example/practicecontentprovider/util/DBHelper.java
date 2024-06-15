016fb031a5dccc8cc3487cac3c2dc19b8537786c
sparkserene <ryan1825114@gmail.com>
2024-06-15 14:36:14 +0800

    implement DBHelper that extends SQLiteOpenHelper to create and delete table

--- app/src/main/java/com/example/practicecontentprovider/util/DBHelper.java
+++ app/src/main/java/com/example/practicecontentprovider/util/DBHelper.java
@@ -3,6 +3,7 @@ package com.example.practicecontentprovider.util;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -15,6 +16,9 @@ public class DBHelper extends SQLiteOpenHelper {
             "CREATE TABLE INCOME_MAIN (" +
                     "_ID INTEGER PRIMARY_KEY," +
                     "INCOME_DESCRIPTION TEXT);";
+    // DDL to delete data from the database
+    private static final String INCOME_DELETE_DDL =
+            "DROP TABLE IF EXISTS INCOME_MAIN;";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,11 +26,14 @@ public class DBHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        Toast.makeText(m_context, "DB creates!!", Toast.LENGTH_LONG).show();
+        db.execSQL(INCOME_CREATE_DDL);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Toast.makeText(m_context, "DB upgrades!!", Toast.LENGTH_LONG).show();
+        db.execSQL(INCOME_DELETE_DDL);
+        db.execSQL(INCOME_CREATE_DDL);
     }
 }
